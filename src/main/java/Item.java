e97a5d11be34094adbc402d8978aa183ff43fa8b
eventually-grapes <nanderson7291984@gmail.com>
2025-11-24 18:40:08 -0500

    add GetItemGUI method with upload button

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,8 +1,13 @@
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
 
 public class Item{
     public String name;
@@ -33,5 +38,21 @@ public class Item{
             e.printStackTrace();
         }
         return null;
+    }
+
+    public JPanel getItemGUI() {
+        JPanel itemTile = new JPanel();
+        JButton uploadButton = new JButton();
+        uploadButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+            JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
+            int result = fileChooser.showOpenDialog(null);
+            if (result != 1) { //file was actually selected
+                setImage(fileChooser.getSelectedFile().getAbsolutePath());
+            }
+            }
+        });
+        itemTile.add(uploadButton);
+        return itemTile;
     }
 }
