5e5b1cedc3bd0703c7b4114da87db93fd026c9d9
Ahaba.co <ahaba.co@localhost.localdomain>
2025-11-28 16:59:41 -0500

    added the left panel, tiers are placed in rows, adding somethiing to a tier works, adding alot of items will make the items in the tier to get smaller until it turns into a scrollable thing. adding alot of tiers also makes the tier list scrollable

--- src/main/java/Tier.java
+++ src/main/java/Tier.java
@@ -1,11 +1,12 @@
+import java.awt.Color;
 
 public class Tier{
     // This class is just a data structure
     public ItemList items;
-    public String color;
+    public Color color;
     public String name;
 
-    public Tier(ItemList items, String color, String name){
+    public Tier(ItemList items, Color color, String name){
         this.items = items;
         this.color = color;
         this.name = name;
--- src/main/java/TierList.java
+++ src/main/java/TierList.java
@@ -1,12 +1,18 @@
 import java.util.HashMap;
+import java.awt.Color;
 
 public class TierList{
     public HashMap<Integer, Tier> tiers;
 
     public TierList(){
-         // only adds first tier in by default
+         // adds default tiers S, A, B, C, D, F with white color
         this.tiers = new HashMap<Integer, Tier>();
-        this.tiers.put(0, new Tier(new ItemList(), "000,000,000", "S"));
+        this.tiers.put(0, new Tier(new ItemList(), Color.WHITE, "S"));
+        this.tiers.put(1, new Tier(new ItemList(), Color.WHITE, "A"));
+        this.tiers.put(2, new Tier(new ItemList(), Color.WHITE, "B"));
+        this.tiers.put(3, new Tier(new ItemList(), Color.WHITE, "C"));
+        this.tiers.put(4, new Tier(new ItemList(), Color.WHITE, "D"));
+        this.tiers.put(5, new Tier(new ItemList(), Color.WHITE, "F"));
     }
     public static void addTier(Tier tier){}
     public static void removeTier(int position){}
